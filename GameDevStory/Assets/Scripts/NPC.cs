6bc432054d90f9a52d5d50f21017117ed80aad76
Darcy Cox <darcycox97@gmail.com>
2018-09-28 10:42:45 +1200

    Programmatic instantiation of NPCs

---
+++ GameDevStory/Assets/Scripts/NPC.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour {
+
+    public GameObject[] npcs;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 0; i < npcs.Length; i++)
+        {
+            Instantiate(npcs[i], new Vector3(i * 0.2f, 0, 0f), Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
