0b34c965500076ea531dee75a455cfaed664b2e6
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-09-28 15:43:15 +1200

    Add slider initialisation script

---
+++ GameDevStory/Assets/Scripts/Hiring/InitialiseSlider.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InitialiseSlider : MonoBehaviour {
+
+    private const float SLIDER_MAX_VALUE = 100;
+
+	void Start()
+    {
+        Slider slider = gameObject.GetComponent<Slider>();
+        slider.maxValue = SLIDER_MAX_VALUE;
+
+        slider.value = 40;
+    }
+}
