b2c972339c1d4e0bb331fae05376e6cf1a1e6d05
Edward Zhang <edward@zhang.nz>
2018-09-28 16:57:04 +1200

    Added WIP Main Menu

---
+++ GameDevStory/Assets/Scripts/LoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void LoadByIndex(int index) {
+		SceneManager.LoadScene(index);
+	}
+
+}
