e1c35a9f4b9f09a4ccae1a3bc1836e398da1900d
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-09-28 16:07:50 +1200

    Slider color value based off percentage filled

--- GameDevStory/Assets/Scripts/Hiring/InitialiseSlider.cs
+++ GameDevStory/Assets/Scripts/Hiring/InitialiseSlider.cs
@@ -5,13 +5,22 @@ using UnityEngine.UI;
 
 public class InitialiseSlider : MonoBehaviour {
 
-    private const float SLIDER_MAX_VALUE = 100;
+    public Image Fill; // Assign the fill value for the slider in the editor
 
+    private const float SLIDER_MAX_VALUE = 100;
+    private const float VALUE = 60;
+
+    // TODO: Change from const to file read value
 	void Start()
     {
+
         Slider slider = gameObject.GetComponent<Slider>();
         slider.maxValue = SLIDER_MAX_VALUE;
 
-        slider.value = 40;
+        slider.value = VALUE;
+
+        // Based on value from 0-100 the slider color will fall in agradient from
+        // red - yellow - green.
+        Fill.color =  new Color(0.01f * (100 - VALUE), 0.01f * VALUE, 0);
     }
 }
