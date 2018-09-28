70ecd219d56a50fb3d8486d764c5981790aa7674
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-09-28 21:03:36 +1200

    Have bio in scrollable state

--- GameDevStory/Assets/Scripts/Hiring/InitialiseApplicantView.cs
+++ GameDevStory/Assets/Scripts/Hiring/InitialiseApplicantView.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 
 public class InitialiseApplicantView : MonoBehaviour
 {
     public Sprite sprite;
     public Image image;
+    public TextMeshProUGUI text;
 
     // Use this for initialization
     void Start()
     {
+        text.text = "A paragraph(from the Ancient Greek παράγραφος paragraphos, \"to write beside\" or \"written beside\") is a self - contained unit of a discourse in writing dealing with a particular point or idea. A paragraph consists of one or more sentences.";
         image.sprite = sprite;
     }
 }
