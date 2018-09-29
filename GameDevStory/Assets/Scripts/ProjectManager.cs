babe9e11139a552953ece0bb6376cbeb2bd07aef
Jess_Alcantara <jessica.alcantara97@gmail.com>
2018-09-29 19:23:30 +1200

    Add project progress timer

---
+++ GameDevStory/Assets/Scripts/ProjectManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ProjectStatus
+{
+    NONE, COMPLETE, IN_PROGRESS
+}
+
+public class ProjectManager : MonoBehaviour {
+
+	private ProjectStatus status = ProjectStatus.NONE;
+	private ProjectTimer timerScript;
+	private GameManager gameScript;
+
+	public Button StartButton;
+
+	public void StartProject ()
+	{
+		status = ProjectStatus.IN_PROGRESS;
+		//timerScript = GetComponent <ProjectTimer>();
+
+	}
+
+}
