c4962e6098cac6b1667f571102d25d48a49dc0a1
Darcy Cox <darcycox97@gmail.com>
2018-09-29 20:34:50 +1200

    Communication between game manager and NPC Controller implemented. Scenarios can now be initialized in game when notification is pressed

--- GameDevStory/Assets/Scripts/NPCScripts/NPCController.cs
+++ GameDevStory/Assets/Scripts/NPCScripts/NPCController.cs
@@ -12,6 +12,8 @@ public class NPCController : Singleton<NPCController> {
 
     public GameObject[] npcs;
 
+    private List<GameObject> npcInstances = new List<GameObject>(); // maintain a reference to each npc in the scene
+
 	// Use this for initialization
 	void Start () {
 		float x;
@@ -20,13 +22,27 @@ public class NPCController : Singleton<NPCController> {
         {
 			x = deskPositions[i % 3][0];
 			y = deskPositions[i % 3][1];
-            Instantiate(npcs[i], new Vector3(x, y, 0f), Quaternion.identity);
+            GameObject npcInstance = Instantiate(npcs[i], new Vector3(x, y, 0f), Quaternion.identity);
+            npcInstance.transform.SetParent(this.transform); // npcs should show up as a child of the npc controller
+            npcInstances.Add(npcInstance);
         }
     }
 
     // Sends a scenario notification to an npc that the player should click on to start the scenario.
-    public void PopUpScenario(Scenario s)
+    public void ShowScenarioNotification(Scenario s)
     {
-
+        foreach(GameObject npc in npcInstances)
+        {
+            NPC npcScript = npc.GetComponent<NPC>();
+            if (!npcScript.HasNotification())
+            {
+                npcScript.ShowScenarioNotification(s);
+            }
+            else
+            {
+                // could add scenario to a queue so it shows when it is free, depends on what we decide
+                Debug.Log("No NPC free to accept notification");
+            }
+        }
     }
 }
--- GameDevStory/Assets/Scripts/ScenarioScripts/TestScenarioExecutor.cs
+++ GameDevStory/Assets/Scripts/ScenarioScripts/TestScenarioExecutor.cs
@@ -39,7 +39,8 @@ public class TestScenarioExecutor : AScenarioExecutor {
             }
         };
 
-        DialogueManager.Instance.StartDialogue(testDialogue);
+        //TODO uncomment this once dialogue elements are in the scene
+      //  DialogueManager.Instance.StartDialogue(testDialogue);
 
     }
 
