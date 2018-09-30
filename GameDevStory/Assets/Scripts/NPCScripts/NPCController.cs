1e061cefa97649a681f00190dbb4e70e1c5032b9
Unknown <crai897@aucklanduni.ac.nz>
2018-09-30 14:53:35 +1200

    Add coordinate system
    * Add office layout system
    * Game coordinates are used to specify npc and office layout
    Peer programming Cyrus & Liam

--- GameDevStory/Assets/Scripts/NPCScripts/NPCController.cs
+++ GameDevStory/Assets/Scripts/NPCScripts/NPCController.cs
@@ -2,36 +2,48 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class NPCController : Singleton<NPCController> { 
 
-	public static float[][] deskPositions = {
-		new float[]{0.06f,-0.06f},
-		new float[]{0.08f,-0.37f},
-		new float[]{-0.24f,-0.21f}
-	};
+    // for npcs, but where the desk are.
+	public static Vector2[] npcPositions = {
+        new Vector2(0,0),
+        new Vector2(0,4),
+        new Vector2(2.5f,1),
+		new Vector2(2.5f,3),
+        new Vector2(1.5f,1)
+    };
 
     public GameObject npcTemplate; // the generic npc template to instantiate
     public RuntimeAnimatorController[] animatorControllers; // pool of possible animations to give the generated npc
+
+	public CoordinateSystem coordinateSystem;
+
+	private GameObject InstantiateNPC(RuntimeAnimatorController animation, Vector2 position)
+	{
+		Vector3 pos = coordinateSystem.getVector3(position);
+		GameObject npcInstance = Instantiate(npcTemplate, pos, Quaternion.identity);
+		npcInstance.GetComponent<Animator>().runtimeAnimatorController = animation; // set the animator controller
+        npcInstance.transform.SetParent(this.transform); // npcs should show up as a child of the npc controller
+		npcInstances.Add(npcInstance);
+		return npcInstance;
+	}
 
     private List<GameObject> npcInstances = new List<GameObject>(); // maintain a reference to each npc in the scene
 
 	// Use this for initialization
 	void Start () {
-		float x;
-		float y;
 
-        for (int i = 0; i < animatorControllers.Length; i++)
+		for (int i = 0; i < npcPositions.Length; i++)
         {
-            GameObject npcInstance = Instantiate(npcTemplate, new Vector3(-0.6f + i * 0.2f, -0.05f, 0), Quaternion.identity);
-            npcInstance.GetComponent<Animator>().runtimeAnimatorController = animatorControllers[i]; // set the animator controller
-            npcInstance.transform.SetParent(this.transform); // npcs should show up as a child of the npc controller
-            npcInstances.Add(npcInstance);
+			InstantiateNPC(animatorControllers[i % animatorControllers.Length], npcPositions[i]);
         }
     }
 
     // Sends a scenario notification to an npc that the player should click on to start the scenario.
     public void ShowScenarioNotification(Scenario s)
     {
+		/*
         foreach(GameObject npc in npcInstances)
         {
             NPC npcScript = npc.GetComponent<NPC>();
@@ -45,5 +57,6 @@ public class NPCController : Singleton<NPCController> {
                 Debug.Log("No NPC free to accept notification");
             }
         }
+        */
     }
 }
