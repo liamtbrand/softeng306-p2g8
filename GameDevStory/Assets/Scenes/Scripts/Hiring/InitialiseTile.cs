7be8708dd4fb7ca26e2464e075f40a6958882a25
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-01 14:11:37 +1300

    Begin to merge hiring views with each other

---
+++ GameDevStory/Assets/Scenes/Scripts/Hiring/InitialiseTile.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class InitialiseTile : MonoBehaviour {
+
+    // Misc. character info
+    public GameObject image;
+    public TextMeshProUGUI nameHeader;
+    public TextMeshProUGUI ageHeader;
+    public TextMeshProUGUI genderHeader;
+
+    // Use this for initialization
+    void Start()
+    {
+
+        // Get the npc's stat from their stats script
+        var npc = NPCFactory.Instance.CreateNPCWithRandomizedStats();
+        image.GetComponent<Animator>().runtimeAnimatorController = npc.attributes.animationController;
+        //spriteImage.sprite = npc.attributes.sprite;
+        nameHeader.text = npc.attributes.name;
+        ageHeader.text = npc.attributes.age.ToString();
+        genderHeader.text = npc.attributes.gender.ToString();
+    }
+
+    void Update()
+    {   
+    }
+
+}
