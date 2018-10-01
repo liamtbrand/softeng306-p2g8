e302ebc51f4485e2c584d0204aaa6a966f206af5
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-02 01:24:26 +1300

    (WIP) Add hiring views to prototype scene, add grid display functionality

---
+++ GameDevStory/Assets/Scripts/Hiring/HiringDisplayManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiringDisplayManager : MonoBehaviour {
+
+    public GameObject GridViewPanel;
+    public GameObject ApplicantViewPanel;
+
+    public void ShowHiringGrid()
+    {
+        GridViewPanel.SetActive(true);
+    }
+
+    public void CloseHiringGrid()
+    {
+        GridViewPanel.SetActive(false);
+    }
+}
