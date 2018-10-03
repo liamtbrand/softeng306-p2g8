8d6c612601add850015f54ab1f6b15adcca6c9d7
Liam Brand <liamtbrand@gmail.com>
2018-10-03 18:36:16 +1300

    Add comments to OfficeLayout

--- GameDevStory/Assets/Scripts/ScenarioScripts/OfficeLayout.cs
+++ GameDevStory/Assets/Scripts/ScenarioScripts/OfficeLayout.cs
@@ -1,6 +1,11 @@
 ﻿using UnityEngine;
 using System;
 
+/// This class can be used to setup an office layout in the scenario.
+/// A desk prefab is attached via the unity editor for instantiation.
+/// A coordinate system is also attached via the editor.
+/// Desk positions are specified, and these get instantiated to the screen
+/// when in-game.
 public class OfficeLayout : MonoBehaviour
 {
 	public CoordinateSystem coordinateSystem;
@@ -20,7 +25,7 @@ public class OfficeLayout : MonoBehaviour
     // Use this for initialization
     void Start()
 	{
-    
+
 		for (int i = 0; i < desks.Length; i++)
         {
             Vector3 pos = coordinateSystem.getVector3(desks[i]);
