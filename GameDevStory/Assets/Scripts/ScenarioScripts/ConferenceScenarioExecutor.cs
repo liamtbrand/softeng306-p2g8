acbf83e80498841ba163613405986aab8cb8e3e7
Cameron Scoular <cameron.scoular1@gmail.com>
2018-10-03 13:06:52 +1300

    commit before merge

---
+++ GameDevStory/Assets/Scripts/ScenarioScripts/ConferenceScenarioExecutor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DialogueScripts;
+
+public class ConferenceScenarioExecutor : AScenarioExecutor
+{
+
+	public Dialogue dialogue1;
+	public Dialogue dialogue2;
+    public override void execute()
+    {
+        
+		DialogueManager.Instance.StartDialogue(dialogue1);
+		GameManager.Instance.fadeToBlack();
+		DialogueManager.Instance.QueueDialogue(dialogue2);
+		
+    }
+}
