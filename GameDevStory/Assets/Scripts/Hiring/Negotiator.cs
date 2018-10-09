077a4cb3f97135a0cfe6c4fc9cb11985ad035253
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-09 16:48:01 +1300

    Integrate negotiating into scene

--- GameDevStory/Assets/Scripts/Hiring/Negotiator.cs
+++ GameDevStory/Assets/Scripts/Hiring/Negotiator.cs
@@ -6,6 +6,7 @@ using TMPro;
 public class Negotiator : MonoBehaviour
 {
     public static NPCInfo npc;
+    public static Button ClickedTile;
 
     public Slider PaySlider;
     public TextMeshProUGUI CostDisplay;
@@ -13,6 +14,11 @@ public class Negotiator : MonoBehaviour
 
 
     public void Start()
+    {
+        Reload();
+    }
+
+    public void Reload()
     {
         // Offer slider starts out at npc's optimum value.
         PaySlider.value = npc.Attributes.cost;
@@ -37,16 +43,22 @@ public class Negotiator : MonoBehaviour
      */
     public void Negotiate()
     {
+        Debug.Log(npc.Attributes.costThreshold);
         var offer = PaySlider.value; // Get the user's offer from the pay slider
         var offerThreshold = npc.Attributes.costThreshold;
 
         if (offer >= offerThreshold)
-        {
+        { 
+            Debug.Log(string.Format("Offer of {0} is sufficient, threshold was {1}", offer.ToString(), offerThreshold.ToString()));
 
+            // Offer is sufficient, applicant can be hired.
+            NPCController.Instance.HireEmployee(npc);
+            GameManager.Instance.changeBalance(npc.Attributes.cost * -1);
+            ClickedTile.interactable = false;
         }
         else
         {
-
+            Debug.Log(string.Format("Offer of {0} is NOT sufficient, threshold was {1}", offer.ToString(), offerThreshold.ToString()));
         }
     }
 }
