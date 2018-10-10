f6d77af7a787c4d6697cf559d9fd09db467384e9
Molly Farrant <mollybfarrant@gmail.com>
2018-10-10 14:56:00 +1300

    Dropdown lists now render in front of image

---
+++ GameDevStory/Assets/Scripts/Hiring/JobAdManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class JobAdManager : MonoBehaviour {
+
+    // The job advertisement screen to be shown
+    public GameObject jobAdEditor;
+
+    void Start() {
+        jobAdEditor.SetActive(false);
+    }
+
+    // Display the job advertisement editor
+    public void ShowEditor()
+    {
+        jobAdEditor.SetActive(true);
+    }
+
+    // This should be called to start the first level
+    public void CloseEditor()
+    {
+        jobAdEditor.SetActive(false);
+    }
+
+}
