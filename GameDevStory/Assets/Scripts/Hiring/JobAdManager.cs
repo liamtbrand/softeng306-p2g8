4764d268ae45c218ca379a92e4a44aff39ec7ce2
Molly Farrant <mollybfarrant@gmail.com>
2018-10-10 15:31:58 +1300

    Methods added to calculate what proportion of applicants will be female

--- GameDevStory/Assets/Scripts/Hiring/JobAdManager.cs
+++ GameDevStory/Assets/Scripts/Hiring/JobAdManager.cs
@@ -6,8 +6,28 @@ using System.Collections.Generic;
 
 public class JobAdManager : MonoBehaviour {
 
+    
     // The job advertisement screen to be shown
     public GameObject jobAdEditor;
+
+    private List<string> feminineWords = new List<string>() {
+        "community of engineers", 
+        "family of coders",
+        "teamwork",
+        "communication",
+        "flexibility",
+        "transparency",
+        "fostering meaningful relationships",
+        "satisfied",
+        "happy",
+        "proficient",
+        "experienced",
+        "team",
+        "collaborative",
+        "natural",
+        "solve",
+        "passionate",
+        "fantastic"};
 
     void Start() {
         jobAdEditor.SetActive(false);
@@ -23,6 +43,39 @@ public class JobAdManager : MonoBehaviour {
     public void CloseEditor()
     {
         jobAdEditor.SetActive(false);
+    }
+
+    public void Advertise() {
+        List<String> values = GetDropdownValues();
+        double proportion = GetFemaleApplicantProportion(values);
+        // TODO: Link this to percentage of female / male applicants in hiring grid
+    }
+
+    public List<string> GetDropdownValues() 
+    {
+        List<string> selectedItems = new List<string>();
+        Dropdown[] dropdownList = this.gameObject.GetComponents<Dropdown>();
+
+        foreach (Dropdown d in dropdownList) {
+            int menuIndex = d.value;
+            List<Dropdown.OptionData> menuOptions = d.options;
+            selectedItems.Add(menuOptions[menuIndex].text);
+        }
+
+        return selectedItems;
+    }
+
+    public double GetFemaleApplicantProportion(List<string> values) {
+
+        double proportion = 0;
+
+        foreach (string selectedWord in values) {
+            if (feminineWords.Contains(selectedWord)) {
+                // Add 10% for each 'feminine' word selected
+                proportion += 0.1;
+            }
+        }
+        return proportion;
     }
 
 }
