3911e006b2e1286e1fbf740d96e8d16b13a041a9
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-12 10:44:06 +1300

    Add employee view scene, programatically add arbitrary list elements

---
+++ GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PopulateEmployeeScroll : MonoBehaviour {
+
+    public GameObject Content;
+    public GameObject EmployeeItemTemplate;
+
+    public void Start()
+    {
+        Reload();
+    }
+
+    public void Reload()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var copy = Instantiate(EmployeeItemTemplate);
+            copy.transform.parent = Content.transform;
+        }
+    }
+
+}
