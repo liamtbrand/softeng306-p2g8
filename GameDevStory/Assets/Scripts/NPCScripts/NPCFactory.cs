6f85efd266d3459dfd1ede3dab2dfc014f57f700
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-11 15:56:38 +1300

    Negotiation frustration level is now a field exclusive to an NPC - state is retained

--- GameDevStory/Assets/Scripts/NPCScripts/NPCFactory.cs
+++ GameDevStory/Assets/Scripts/NPCScripts/NPCFactory.cs
@@ -81,6 +81,7 @@ public class NPCFactory : Singleton<NPCFactory> {
 
         npc.Attributes.cost = CalculateEmployeeCost(npc);
         npc.Attributes.costThreshold = CalculateCostThreshold(npc);
+        npc.Attributes.negotiationFrustration = 0;
 
         return npc;
     }
