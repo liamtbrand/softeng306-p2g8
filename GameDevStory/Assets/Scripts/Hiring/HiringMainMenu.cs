bea1a756b56dc5c9cdb71f7e5b7e3d21e9c1630b
Darcy Cox <darcycox97@gmail.com>
2018-10-12 23:52:38 +1300

    Create UI for Hiring main menu

---
+++ GameDevStory/Assets/Scripts/Hiring/HiringMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HiringMainMenu : MonoBehaviour {
+
+    public GameObject HiringMenuWindow;
+
+	// Use this for initialization
+	void Start () {
+        HiringMenuWindow.SetActive(false);
+	}
+	
+	void ShowHiringMenu()
+    {
+        HiringMenuWindow.SetActive(true);
+    }
+}
