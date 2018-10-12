87250f90cca67a110eb68e1d54af0aef7c9397b2
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-12 14:25:52 +1300

    Tetsing populates elements uniquely

---
+++ GameDevStory/Assets/Scripts/UIScripts/EmployeeScrollElement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EmployeeScrollElement : MonoBehaviour {
+
+    public TextMeshProUGUI NameLabel;
+    public TextMeshProUGUI AgeLabel;
+    public TextMeshProUGUI GenderLabel;
+    public TextMeshProUGUI PaidLabel;
+
+    public NPCInfo Npc { get; set; }
+
+    public void Populate()
+    {
+        NameLabel.text = Npc.Attributes.npcName;
+        AgeLabel.text = Npc.Attributes.age.ToString();
+        GenderLabel.text = Npc.Attributes.gender.ToString();
+        // TODO: FIll in aid for element
+    }
+}
--- GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
+++ GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
@@ -22,12 +22,28 @@ public class PopulateEmployeeScroll : MonoBehaviour {
         //    copy.GetComponent<TextMeshProUGUI>().text = NpcPair.Value.Attributes.npcName;
         //    copy.transform.parent = Content.transform;
         //}
+        var name = "a";
+        var cost = 0;
+        var age = 50;
         for (int i = 0; i < 10; i++)
         {
             var copy = Instantiate(EmployeeItemTemplate);
             copy.transform.parent = Content.transform;
 
-            
+            var npc = new NPCInfo();
+            npc.Attributes = new NPCAttributes();
+            npc.Attributes.npcName = name;
+            npc.Attributes.gender = NPCAttributes.Gender.FEMALE;
+            npc.Attributes.cost = cost;
+            npc.Attributes.age = age;
+
+            var Element = copy.GetComponent<EmployeeScrollElement>();
+            Element.Npc = npc;
+            Element.Populate();
+
+            name += "a";
+            cost++;
+            age++;
         }
     }
 
