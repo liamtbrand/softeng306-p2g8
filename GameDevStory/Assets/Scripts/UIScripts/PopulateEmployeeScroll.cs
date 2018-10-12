476a732bbde6bfd56827fb04492d3e0e7c709cf4
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-12 13:58:52 +1300

    Add specialised ui panel for employees and add features to panel

--- GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
+++ GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
@@ -9,16 +9,25 @@ public class PopulateEmployeeScroll : MonoBehaviour {
 
     public void Start()
     {
+ 
         Reload();
     }
 
     public void Reload()
     {
-        foreach(KeyValuePair<GameObject, NPCInfo> NpcPair in NPCController.Instance.NpcInstances)
+        // For actually loopig through the game's npcs.
+        //foreach(KeyValuePair<GameObject, NPCInfo> NpcPair in NPCController.Instance.NpcInstances)
+        //{
+        //    var copy = Instantiate(EmployeeItemTemplate);
+        //    copy.GetComponent<TextMeshProUGUI>().text = NpcPair.Value.Attributes.npcName;
+        //    copy.transform.parent = Content.transform;
+        //}
+        for (int i = 0; i < 10; i++)
         {
             var copy = Instantiate(EmployeeItemTemplate);
-            copy.GetComponent<TextMeshProUGUI>().text = NpcPair.Value.Attributes.npcName;
             copy.transform.parent = Content.transform;
+
+            
         }
     }
 
