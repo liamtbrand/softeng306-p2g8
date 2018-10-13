f590054e4bff51f7dee1e44d7d4c8d2bd81d9b6f
Buster Darragh-Major <busterjdarraghmajor@gmail.com>
2018-10-13 18:23:27 +1300

    Employee average rating calculation added

--- GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
+++ GameDevStory/Assets/Scripts/UIScripts/PopulateEmployeeScroll.cs
@@ -22,26 +22,51 @@ public class PopulateEmployeeScroll : MonoBehaviour {
 
     public void Reload()
     {
+        // Clear scroll view for reload
         ClearElements(ScrollViewContent);
 
-        // Populate statistics
-        TotalLabel.text = "69";
-        TotalSpentLabel.text = "$" + "4000";
-        AverageAgeLabel.text = "69";
-        AverageRatingLabel.text = "4.5";
+        int totalEmployees = 0;
+        int totalSpent = 0;
+        int totalAge = 0;
+        float totalRating = 0;
 
-        // For actually loopig through the game's npcs.
+        // Loop through all currently hired npcs
         foreach (KeyValuePair<GameObject, NPCInfo> NpcPair in NPCController.Instance.NpcInstances)
         {
+            // Add a new item to the scroll view
             var copy = Instantiate(EmployeeItemTemplate);
             copy.transform.SetParent(ScrollViewContent.transform, false);
 
             var Element = copy.GetComponent<EmployeeScrollElement>();
             Element.Npc = NpcPair.Value;
             Element.Populate();
+
+            // Record some stats
+            totalEmployees++;
+            totalSpent += NpcPair.Value.Attributes.ammountPaidFor;
+            totalAge += NpcPair.Value.Attributes.age;
+
+            // Rating takes values from all stats for a total out of 500.
+            var totalForEmployee = NpcPair.Value.Stats.Communication + 
+                NpcPair.Value.Stats.Creativity + 
+                NpcPair.Value.Stats.Design + 
+                NpcPair.Value.Stats.Technical + 
+                NpcPair.Value.Stats.Testing;
+
+            totalRating += totalForEmployee / 100f; // Normalize to out of 5.
+
         }
+
+        // Populate statistics
+        TotalLabel.text = totalEmployees.ToString();
+        TotalSpentLabel.text = "$" + totalSpent.ToString();
+        AverageAgeLabel.text = (totalAge / (float) totalEmployees).ToString();
+        AverageRatingLabel.text = (totalRating / totalEmployees).ToString("n2");
     }
 
+    /**
+     * Removes the current set of employee elements in the scroll view.
+     */
     private void ClearElements(GameObject scrollViewContent)
     {
         foreach (Transform child in scrollViewContent.transform)
